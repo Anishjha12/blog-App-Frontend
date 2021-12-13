53f83c293d9d46bd7d3dc6a2b8af9df274a7877e
Anish <jhaanish891@gmail.com>
2021-12-13 22:44:47 +0530

    Write/Single post Page added

---
+++ src/Pages/writes/Write.jsx
@@ -0,0 +1,31 @@
+import "./write.css"
+
+export default function write() {
+    return (
+        <div className="write">
+            <img className="writeImg" 
+            src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=500" alt=""/>
+            <form className="writeform">
+                <div className="writeFormGroup">
+                    <label htmlFor="fileInput">
+                        <i className="writeIcon fas fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{ display: "none" }} />
+                    <input 
+                    type="text" 
+                    placeholder="Title" 
+                    className="writeInput" 
+                    autoFocus={true} 
+                    />
+                </div>
+                <div className="writeFormGroup">
+                    <textarea placeholder="Tell your story........."
+                        type="text"
+                        className="writeInput writeText"
+                    ></textarea>
+                </div>
+                <button className="writeSubmit">Publish</button>
+            </form>
+        </div>
+    )
+}
